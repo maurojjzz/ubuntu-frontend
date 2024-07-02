4c384731557749c220ff44e5b5384f0f21848d03
Pablo Galindo <pablogalindo90@gmail.com>
2024-07-02 12:40:03 -0300

    Creada la estructura base para las tarjetas y la vista basica de publicaciones

--- ubuntu-frontend/src/App.jsx
+++ ubuntu-frontend/src/App.jsx
@@ -2,20 +2,22 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ViewMicroEmprendimientos from "./components/view/microEmprendimientos/ViewMicroEmprendimientos";
 import { Typography } from "@mui/material";
 import Login from "./components/view/login/Login";
+import ViewPublicaciones from "./components/view/publicaciones/ViewPublicaciones";
 
 function App() {
   return (
-    <Router>
-      <div>
-      <Typography variant="h1" color="primary-grisOscuro">
-          Ubuntu
-        </Typography>
-        <Routes>
-          <Route path="/microemprendimientos" element={<ViewMicroEmprendimientos />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
+    // <Router>
+    //   <div>
+    //   <Typography variant="h1" color="primary-grisOscuro">
+    //       Ubuntu
+    //     </Typography>
+    //     <Routes>
+    //       <Route path="/microemprendimientos" element={<ViewMicroEmprendimientos />} />
+    //       <Route path="/login" element={<Login />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
+    <ViewPublicaciones></ViewPublicaciones>
   );
 }
 
---
+++ ubuntu-frontend/src/components/Cards/PublicacionesCard.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Card, CardContent, Typography, Button } from '@mui/material';
+
+
+
+const PublicacionesCard = ({ title, reel, date, text }) => {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    const paragraphs = text.split('\n\n');
+    const firstParagraph = paragraphs[0];
+    const remainingParagraphs = paragraphs.slice(1).join('\n\n');
+
+    return (
+        <Card sx={{ maxWidth: 345, margin: '16px auto' }}>
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    {title}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    {date}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {reel}
+                </Typography>
+                <Typography variant="body1" component="p">
+                    {firstParagraph}
+                </Typography>
+                {expanded && (
+                    <Typography variant="body1" component="p">
+                        {remainingParagraphs}
+                    </Typography>
+                )}
+                <Button sx={{color: 'black'}} onClick={handleExpandClick}>
+                    {expanded ? 'Mostrar menos' : 'Leer más'}
+                </Button>
+            </CardContent>
+        </Card>
+    );
+};
+
+PublicacionesCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    reel: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+};
+
+export default PublicacionesCard;
