b19bd2f08afb366ab2e0b9fc0c388a069bc37491
MASATNAFEL <lanixper@gmail.com>
2024-07-04 20:24:45 -0500

    implementacion de la vista de busqueda

--- ubuntu-frontend/src/App.jsx
+++ ubuntu-frontend/src/App.jsx
@@ -2,24 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ViewMicroEmprendimientos from "./components/view/microEmprendimientos/ViewMicroEmprendimientos";
 import LandingPage from "./components/view/landing/LandingPage";
 import Login from "./components/view/login/Login";
+import ViewSearchBar from "./components/view/Searchbar/ViewSearchBar";
 
 function App() {
   return (
     <Router>
       <div>
-          <Routes>
-          <Route
-            path="/"
-            element={<LandingPage />}
-          />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
           <Route
             path="/microemprendimientos"
             element={<ViewMicroEmprendimientos />}
           />
-          <Route
-            path="/login"
-            element={<Login />}
-          />
+          <Route path="/buscar" element={<ViewSearchBar />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
