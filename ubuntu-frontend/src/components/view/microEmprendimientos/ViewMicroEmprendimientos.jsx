c4aa1d5c36ae9f6a9726f6e92e3ba9ab0bec1409
porta.walter <walterportadev@gmail.com>
2024-07-07 22:16:18 -0300

    serviceHttp, falta mejorar tema de roles en las solicitudes

--- ubuntu-frontend/src/components/view/microEmprendimientos/ViewMicroEmprendimientos.jsx
+++ ubuntu-frontend/src/components/view/microEmprendimientos/ViewMicroEmprendimientos.jsx
@@ -1,7 +1,7 @@
 import { useTheme } from "@mui/material/styles";
 import { Box, Typography, Container, Grid } from "@mui/material";
 import data from "../../../../data.json";
-import CustomCard from "../../cards/MicroEmpCard";
+import CustomCard from "../../cards/microEmpCard";
 import imagenmicro from "../../../assets/img/imagen microemprendimientos.jpg";
 import SearchBar from "../../searchBar/SearchBar";
 import Navbar from "../../navbar/Navbar";
