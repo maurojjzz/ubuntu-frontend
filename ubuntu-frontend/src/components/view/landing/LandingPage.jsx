7956930c414f89df858b10009aef49e8b3b95f1e
PC <alonngadea@gmail.com>
2024-07-10 10:50:22 -0300

    Refactor changes in import svg

--- ubuntu-frontend/src/components/view/landing/LandingPage.jsx
+++ ubuntu-frontend/src/components/view/landing/LandingPage.jsx
@@ -5,7 +5,6 @@ import SearchBar from "../../searchBar/SearchBar";
 import backgroundImage from "../../../assets/img/imagenlanding.jpg";
 import Navbar from "../../navbar/Navbar";
 import CategoryItem from "../../categoryItem/CategoryItem";
-import SvgStyle from "../../svg/CategoriesSvg";
 import CircleIcon from "@mui/icons-material/Circle";
 import jsonData from "../../../assets/json/publicaciones.json";
 import PublicacionesCard from "../../cards/PublicacionesCard.jsx";
@@ -166,7 +165,6 @@ const LandingPage = () => {
           alignItems: "center",
         }}
       >
-        <SvgStyle />
         <Typography
           variant="body1"
           align="center"
