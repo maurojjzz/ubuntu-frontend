c73690de61753880b723e6aea676136c1900acae
maurojjzz <maurojim123@gmail.com>
2024-07-16 16:21:03 -0300

    creacion vistas de admin mas componentes

---
+++ ubuntu-frontend/src/Components/microbusinessCard/MicrobusinessCard.jsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+const MicrobusinessCard = () => {
+  return (
+    <Box
+      sx={{
+        width: "70%",
+        minWidth: "328px",
+        maxWidth: "500px",
+        height: "136px",
+        border: "1px solid black",
+      }}
+    >
+      cuadro
+    </Box>
+  );
+};
+
+export default MicrobusinessCard;
---
+++ ubuntu-frontend/src/Components/view/admin/microemprendimiento/Microemprendimiento.jsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from "@mui/material";
+import MicrobusinessCard from "../../../microbusinessCard/MicrobusinessCard";
+
+const Microemprendimiento = () => {
+  return (
+    <Box>
+        <Typography variant="h4">
+            Microemprendimientos
+        </Typography>
+        <MicrobusinessCard />
+    </Box>
+  );
+};
+
+export default Microemprendimiento;
