d7a0833d25204e9cc9e05bdf7db35d6beff9f32c
PC <alonngadea@gmail.com>
2024-07-18 23:57:52 -0300

    Add Changes

--- ubuntu-frontend/src/Components/Navbar/showUser.jsx
+++ ubuntu-frontend/src/Components/Navbar/showUser.jsx
@@ -1,12 +1,13 @@
 import { Box } from "@mui/material"
 
 
+
 const ShowMyGoogleUserName = () => {
     return (
         <Box 
-        sx={{ position: "relative" }}
+        sx={{  }}
         className="nav__User">
-            <div className="show-datauser" style={{ backgroundColor: 'black', padding: "8px", borderRadius: 100, color: "white"}}>
+            <div className="show-datauser" style={{ backgroundColor: 'black', padding: "8px", borderRadius: 100, color: "white", display: "flex", gap: "8px"}}>
                 AD
             </div>
         </Box>
