b2a96e68dc9e41099536b183782aefd3c295edc5
Pablo Galindo <pablogalindo90@gmail.com>
2024-07-31 12:33:47 -0300

    maquetado inicial de la vista solicitud de contacto

---
+++ ubuntu-frontend/src/Components/view/admin/solicitudContacto/SolicitudContacto.jsx
@@ -0,0 +1,66 @@
+import { Box, Typography } from "@mui/material";
+import theme from "../../../../theme/theme";
+
+function SolicitudContacto() {
+
+    return (
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "3vh",
+            paddingTop: "3vh",
+            paddingLeft: "3vw",
+            paddingRight: "3vw",
+        }} className="solicitudContacto__Container">
+            <Box className="solicitudContacto__Title">
+                <Typography sx={{
+                    fontFamily: 'Lato',
+                    fontSize: '28px',
+                    fontWeight: '500',
+                    lineHeight: '35px',
+                }}>Solicitudes de contacto</Typography>
+            </Box>
+            <Box sx={{ display: "flex", 
+                flexDirection: "rows",
+                borderBottom: "solid 1px",
+                borderColor: theme.palette.primary.azul,
+                width: "100%",
+                padding: '8px 10px 20px 10px',
+            }}>
+                <Box sx={{
+                    width: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}>
+                    <Typography sx={{
+                        fontFamily: 'Lato',
+                        fontWeight: '700',
+                        fontSize: '16px',
+                        lineHeight: '20px'
+                    }}>No gestionadas</Typography>
+                </Box>
+                <Box sx={{
+                    width: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}>
+                    <Typography sx={{
+                        fontFamily: 'Lato',
+                        fontWeight: '700',
+                        fontSize: '16px',
+                        lineHeight: '20px'
+                    }}>Gestionadas</Typography>
+                </Box>
+            </Box>
+            <Box>
+                Tarjetas
+            </Box>
+        </Box>
+    );
+}
+
+export default SolicitudContacto;
