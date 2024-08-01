be6492e2be6457872ed8318734080d30e3ca1999
maurojjzz <maurojim123@gmail.com>
2024-08-01 10:44:21 -0300

    mockup done

--- ubuntu-frontend/src/Components/shared/modalAlert/ModalAlert.jsx
+++ ubuntu-frontend/src/Components/shared/modalAlert/ModalAlert.jsx
@@ -1,16 +1,26 @@
-import { Box, Modal, useTheme } from "@mui/material";
+import { Box, Modal, useTheme, Typography, Button } from "@mui/material";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 
-const ModalAlert = ({type = "error"}) => {
+const ModalAlert = ({ status = "error" }) => {
   const theme = useTheme();
 
   const iconStyle = {
-    color: type === "success" ? theme.palette.primary.verdeGestion : theme.palette.primary.rojo,
-    fontSize: "40px",
+    color: status === "success" ? theme.palette.primary.verdeGestion : theme.palette.primary.rojo,
+    fontSize: "50px",
+    mt: "16px",
   };
 
-  
+  const buttonStyle = {
+    color: theme.palette.primary.azul,
+    fontWeight: "600",
+    fontSize: "14px",
+    lineHeight: "20px",
+    textTransform: "none",
+    fontFamily: "Lato",
+  }
+
+
   return (
     <Modal
       open={true}
@@ -18,13 +28,13 @@ const ModalAlert = ({type = "error"}) => {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-       
       }}
     >
       <Box
         sx={{
           display: "flex",
-          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
           minHeight: "152px",
           minWidth: "328px",
           maxWidth: "500px",
@@ -34,10 +44,41 @@ const ModalAlert = ({type = "error"}) => {
           "&:focus": {
             outline: "none",
           },
+          pb: "10px",
         }}
       >
-         {type === "error" && <CancelOutlinedIcon sx={iconStyle} />}
-         {type === "success" && <CheckCircleOutlineIcon sx={iconStyle} />}
+        {status === "error" && <CancelOutlinedIcon sx={iconStyle} />}
+        {status === "success" && <CheckCircleOutlineIcon sx={iconStyle} />}
+        <Typography
+          variant="p"
+          sx={{
+            mt: "16px",
+            fontWeight: "400",
+            fontFamily: "Lato",
+            fontSize: "18px",
+            lineHeight: "28px",
+            mb: "12px",
+            width: "80%",
+          }}
+        >
+          Some text that should be here
+        </Typography>
+        <Box
+          sx={{
+            width: "90%",
+            display: "flex",
+            justifyContent: "end",
+          }}
+        >
+          <Button variant="text" sx={buttonStyle} >
+            {status==="success" ? "Acepar" : "Cancelar"}
+          </Button>
+          {status === "error" && (
+            <Button variant="text" sx={buttonStyle} >
+              Intentar nuevamente
+            </Button>
+          )}
+        </Box>
       </Box>
     </Modal>
   );
