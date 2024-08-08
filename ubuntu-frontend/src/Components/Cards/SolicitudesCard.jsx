24396014dd296923d45b0df59a6cf7f6a835f3a5
Pablo Galindo <pablogalindo90@gmail.com>
2024-08-08 00:27:21 -0300

    Implementada la funcionalidad con el backend

--- ubuntu-frontend/src/Components/Cards/SolicitudesCard.jsx
+++ ubuntu-frontend/src/Components/Cards/SolicitudesCard.jsx
@@ -1,12 +1,12 @@
-import PropTypes from "prop-types";
 import { Card, CardContent, Button, Box, Divider } from "@mui/material";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { useTheme } from "@mui/material/styles";
 import "./SolicitudesCard.css";
 
-const SolicitudesCard = ({ title, date, status, name, surname, email, phone, text, onButtonClick }) => {
+const SolicitudesCard = ({ title, dateCreated, dateUpdated, status, name, surname, email, phone, text, onButtonClick }) => {
     const theme = useTheme();
-    const className = status === "unprocessed" ? "solicitudesCard__orangeDot" : "solicitudesCard__greenDot";
+    const className = status === false ? "solicitudesCard__orangeDot" : "solicitudesCard__greenDot";
+    const date = status === false ? dateCreated : dateUpdated
 
     return (
         <Card sx={{
@@ -66,25 +66,15 @@ const SolicitudesCard = ({ title, date, status, name, surname, email, phone, tex
                         textTransform: 'none',
                         minWidth: '24px'
                     }}
-                    onClick={() => onButtonClick({ title, date, status, name, surname, email, phone, text })}>
+                    onClick={() => 
+                        onButtonClick({ title, dateCreated, dateUpdated, status, name, surname, email, phone, text })
+                    }>
                         <ArrowForwardIosIcon />
                     </Button>
                 </Box>
             </CardContent>
         </Card>
     );
-};
-
-SolicitudesCard.propTypes = {
-    title: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-    name: PropTypes.string,
-    surname: PropTypes.string,
-    email: PropTypes.string,
-    phone: PropTypes.string,
-    text: PropTypes.string,
-    onButtonClick: PropTypes.func.isRequired,
 };
 
 export default SolicitudesCard;
