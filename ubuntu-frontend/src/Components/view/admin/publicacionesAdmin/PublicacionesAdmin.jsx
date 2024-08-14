6c66574c5fa93f283c00bbbcfb4076e6a1108848
Pablo Galindo <pablogalindo90@gmail.com>
2024-08-14 11:22:06 -0300

    agregue el param id al componente cuando se busca en el searchbar para que funcione correctamente

--- ubuntu-frontend/src/Components/view/admin/publicacionesAdmin/PublicacionesAdmin.jsx
+++ ubuntu-frontend/src/Components/view/admin/publicacionesAdmin/PublicacionesAdmin.jsx
@@ -1,6 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
+import { ButtonLoad } from "../../../shared";
+import jsonData from '../../../../assets/json/publicaciones.json';
+import PublicacionesCard from "../../../cards/PublicacionesCard";
+import { useState, useEffect } from "react";
 
 const PublicacionesAdmin = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        setData(jsonData);
+    }, []);
+
     return (
         <Box sx={{
             display: "flex",
@@ -24,6 +35,31 @@ const PublicacionesAdmin = () => {
                 >
                     Publicaciones
                 </Typography>
+            </Box>
+            <ButtonLoad btnText="Crear Publicación" btnLink="/admin/publicaciones" />
+            <Box sx={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center', gap: '2vh'}}>
+                <Typography
+                    sx={{
+                        fontFamily: 'Lato',
+                        fontSize: '22px',
+                        fontWeight: '600',
+                        lineHeight: '25px',
+                    }}
+                >
+                    Publicaciones cargadas
+                </Typography>
+                <Grid container spacing={2} sx={{ position: "relative", zIndex: 2 }}>
+                    {data.map((publicacion, index) => (
+                        <Grid item xs={12} sm={6} md={4} key={index}>
+                            <PublicacionesCard
+                                title={publicacion.title}
+                                images={publicacion.images}
+                                date={publicacion.date}
+                                text={publicacion.text}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
             </Box>
         </Box>
     );
